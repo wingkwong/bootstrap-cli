170e63a360cc35e93c26dc1375eb6a66611c2575
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-25 18:45:09 +0800

    feat(common): add constants

---
+++ internal/common/common.go
@@ -0,0 +1,7 @@
+package common
+
+const (
+	NAVIGATION_TEMPLATE_LIST = "navigation-template-list"
+	FRONTEND_TEMPLATE_LIST   = "frontned-template-list"
+	BACKEND_TEMPLATE_LIST    = "backend-template-list"
+)
