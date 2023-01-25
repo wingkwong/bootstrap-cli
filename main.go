3edc95237a8494f69e931fed05e58c29148198fe
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-25 17:12:07 +0800

    chore: move logic to cmd

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/wingkwong/bootstrap-cli/ui"
+	"github.com/wingkwong/bootstrap-cli/cmd"
 )
 
 func main() {
-	m := ui.New()
-	p := tea.NewProgram(m, tea.WithAltScreen())
-
-	if _, err := p.Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
