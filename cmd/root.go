7cd9c771a423e7cc59d41e37c47330f2b6df7847
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-25 17:12:15 +0800

    feat(cmd): root.go

---
+++ cmd/root.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/wingkwong/bootstrap-cli/internal/ui"
+)
+
+func Execute() {
+	p := tea.NewProgram(ui.New(), tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running bootstrap-cli:", err)
+		os.Exit(1)
+	}
+}
