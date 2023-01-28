e29c3deedbfb4aa70df2dedc0f9feb72c8021585
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-28 22:34:20 +0800

    feat(ui): update state

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -1,6 +1,9 @@
 package ui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	_common "github.com/wingkwong/bootstrap-cli/internal/common"
+)
 
 func (b Bubble) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
@@ -8,8 +11,22 @@ func (b Bubble) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmds []tea.Cmd
 	)
 
+	selectedItem := b.navigationList.GetSelectedItem()
+
 	b.navigationList, cmd = b.navigationList.Update(msg)
 	cmds = append(cmds, cmd)
+
+	switch selectedItem.Title() {
+	case _common.FRONTEND_FRAMEWORKS:
+		b.state = frontendTemplateListState
+		// show frontend
+	case _common.BACKEND_FRAMEWORKS:
+		b.state = backendTemplateListState
+	case _common.KUBERNETES_FRAMEWORKS:
+		// show k8s
+	case _common.DOCKER_FRAMEWORKS:
+		// show docker
+	}
 
 	b.frontendTemplateList, cmd = b.frontendTemplateList.Update(msg)
 	cmds = append(cmds, cmd)
