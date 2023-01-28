291808793821c8cc53c6289915a910cbcb7ffe83
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-28 22:31:42 +0800

    feat(common): add constants

--- internal/common/common.go
+++ internal/common/common.go
@@ -1,6 +1,16 @@
 package common
 
 const (
+	FRONTEND_FRAMEWORKS   = "Frontend Frameworks"
+	BACKEND_FRAMEWORKS    = "Backend Frameworks"
+	KUBERNETES_FRAMEWORKS = "Kubernetes"
+	DOCKER_FRAMEWORKS     = "Docker"
+
+	FRONTEND_FRAMEWORKS_DESC   = "Explore Frontend Framework Templates"
+	BACKEND_FRAMEWORKS_DESC    = "Explore Backend Framework Templates"
+	KUBERNETES_FRAMEWORKS_DESC = "Explore Kubernetes Templates"
+	DOCKER_FRAMEWORKS_DESC     = "Explore Dockerfile"
+
 	NAVIGATION_TEMPLATE_LIST = "navigation-template-list"
 	FRONTEND_TEMPLATE_LIST   = "frontned-template-list"
 	BACKEND_TEMPLATE_LIST    = "backend-template-list"
