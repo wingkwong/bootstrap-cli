3496dc20175d7500a5ee14280cf9d4c02bf84d16
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-29 12:57:39 +0800

    feat(list): add title style

--- internal/ui/list/style.go
+++ internal/ui/list/style.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	bubbleStyle     = lipgloss.NewStyle()
-	titleStyle      = lipgloss.NewStyle()
+	titleStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFDF5")).Background(lipgloss.Color("#25A065")).Padding(0, 1)
 	paginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
 	helpStyle       = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
 	quitTextStyle   = lipgloss.NewStyle().Margin(1, 0, 2, 4)
