3122fa962cc508604a73c3772fa69cfdec9909dd
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-29 11:09:14 +0800

    feat(list): use key Matches

--- internal/ui/list/update.go
+++ internal/ui/list/update.go
@@ -1,29 +1,31 @@
 package list
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func (b Bubble) Update(msg tea.Msg) (Bubble, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		b.list.SetWidth(msg.Width)
 		return b, nil
 
 	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "ctrl+c":
-			return b, tea.Quit
-		case "enter":
+		switch {
+		case key.Matches(msg, selectListItemKey):
 			item, ok := b.list.SelectedItem().(Item)
 			if ok {
 				println(item.title)
 				b.choice = item.title
 			}
-			return b, nil
 		}
 	}
 
-	var cmd tea.Cmd
 	b.list, cmd = b.list.Update(msg)
-
+	cmds = append(cmds, cmd)
 	return b, cmd
 }
