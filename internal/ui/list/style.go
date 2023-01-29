66749b7b6ea0c253d804e127d0414aa18bf9b39a
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-29 11:01:21 +0800

    feat(list): add list styles

--- internal/ui/list/style.go
+++ internal/ui/list/style.go
@@ -1,7 +1,15 @@
 package list
 
-import "github.com/charmbracelet/lipgloss"
+import (
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/lipgloss"
+)
 
 var (
-	bubbleStyle = lipgloss.NewStyle()
+	bubbleStyle     = lipgloss.NewStyle()
+	titleStyle      = lipgloss.NewStyle()
+	paginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle       = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle   = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	listHeight      = 20
 )
