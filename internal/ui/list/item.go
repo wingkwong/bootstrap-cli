d9f611be9b2ff1415e7059d03302a74a82497e78
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-29 10:23:18 +0800

    refactor(list): add line breaks

--- internal/ui/list/item.go
+++ internal/ui/list/item.go
@@ -18,15 +18,20 @@ type Item struct {
 	title, desc string
 }
 
-func (i Item) Title() string       { return i.title }
+func (i Item) Title() string { return i.title }
+
 func (i Item) Description() string { return i.desc }
+
 func (i Item) FilterValue() string { return i.title }
 
 type itemDelegate struct{}
 
-func (d itemDelegate) Height() int                               { return 1 }
-func (d itemDelegate) Spacing() int                              { return 0 }
+func (d itemDelegate) Height() int { return 1 }
+
+func (d itemDelegate) Spacing() int { return 0 }
+
 func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+
 func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
 	i, ok := listItem.(Item)
 	if !ok {
