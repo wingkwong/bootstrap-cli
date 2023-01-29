2a3c72aedab8ae0b3944be2b586dcb61bbf48141
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-01-29 10:24:20 +0800

    refactor(list): move out delegate function and add GetChoice

--- internal/ui/list/list.go
+++ internal/ui/list/list.go
@@ -17,11 +17,16 @@ var (
 )
 
 type Bubble struct {
-	list list.Model
+	list   list.Model
+	choice string
 }
 
 func (b Bubble) Init() tea.Cmd {
 	return nil
+}
+
+func (b Bubble) GetChoice() string {
+	return b.choice
 }
 
 func (b Bubble) GetSelectedItem() Item {
@@ -30,6 +35,13 @@ func (b Bubble) GetSelectedItem() Item {
 		return item
 	}
 	return Item{}
+}
+
+func (b *Bubble) SetSize(width, height int) {
+	b.list.SetSize(
+		width,
+		height,
+	)
 }
 
 func (b Bubble) Update(msg tea.Msg) (Bubble, tea.Cmd) {
@@ -46,24 +58,24 @@ func (b Bubble) Update(msg tea.Msg) (Bubble, tea.Cmd) {
 }
 
 func (b Bubble) View() string {
-	return "\n" + b.list.View()
+	return b.list.View()
 }
 
 func New(listType string) Bubble {
 	const defaultWidth = 20
 	var l list.Model
+	var items []list.Item
 	switch listType {
 	case _common.NAVIGATION_TEMPLATE_LIST:
-		items := []list.Item{
+		items = []list.Item{
 			Item{title: _common.FRONTEND_FRAMEWORKS, desc: _common.FRONTEND_FRAMEWORKS_DESC},
 			Item{title: _common.BACKEND_FRAMEWORKS, desc: _common.BACKEND_FRAMEWORKS_DESC},
 			Item{title: _common.KUBERNETES_FRAMEWORKS, desc: _common.KUBERNETES_FRAMEWORKS_DESC},
 			Item{title: _common.DOCKER_FRAMEWORKS, desc: _common.DOCKER_FRAMEWORKS_DESC},
 		}
-		l = list.New(items, list.NewDefaultDelegate(), defaultWidth, listHeight)
 		l.Title = ""
 	case _common.FRONTEND_TEMPLATE_LIST:
-		items := []list.Item{
+		items = []list.Item{
 			Item{title: "vue", desc: "Generate Vue.js App Template"},
 			Item{title: "vue-ts", desc: "Generate Vue.js App Template in TypeScript"},
 			Item{title: "react", desc: "Generate React App Template"},
@@ -75,15 +87,13 @@ func New(listType string) Bubble {
 			Item{title: "gatsby", desc: "Generate Gatsby App Template in TypeScript"},
 			Item{title: "gatsby-ts", desc: "Generate Gatsby App Template in TypeScript"},
 		}
-		l = list.New(items, list.NewDefaultDelegate(), defaultWidth, listHeight)
 		l.Title = "Here's the available Templates."
-
 	case _common.BACKEND_TEMPLATE_LIST:
-		items := []list.Item{}
-		l = list.New(items, list.NewDefaultDelegate(), defaultWidth, listHeight)
+		items = []list.Item{}
 		l.Title = "Here's the available Templates."
 	}
 
+	l = list.New(items, list.NewDefaultDelegate(), defaultWidth, listHeight)
 	l.SetShowStatusBar(false)
 	l.SetFilteringEnabled(true)
 	l.Styles.Title = titleStyle
