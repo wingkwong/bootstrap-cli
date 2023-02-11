8419fd6602684ff8184214fb8d58a80d977a30b7
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-11 21:35:58 +0800

    feat(common): update list title emojis

--- internal/common/common.go
+++ internal/common/common.go
@@ -1,10 +1,10 @@
 package common
 
 const (
-	FRONTEND_FRAMEWORKS   = "✨ Frontend Frameworks"
-	BACKEND_FRAMEWORKS    = "💎 Backend Frameworks"
-	KUBERNETES_FRAMEWORKS = "💡 Kubernetes"
-	DOCKER_FRAMEWORKS     = "🔖 Docker"
+	FRONTEND_FRAMEWORKS   = "📘 Frontend Frameworks"
+	BACKEND_FRAMEWORKS    = "📙 Backend Frameworks"
+	KUBERNETES_FRAMEWORKS = "📕 Kubernetes"
+	DOCKER_FRAMEWORKS     = "📒 Docker"
 
 	FRONTEND_FRAMEWORKS_DESC   = "Explore Frontend Framework Templates"
 	BACKEND_FRAMEWORKS_DESC    = "Explore Backend Framework Templates"
@@ -19,52 +19,52 @@ const (
 	FRONTEND_TEMPLATE_LIST_TITLE   = "Here's the available templates for Frontend Frameworks."
 	BACKEND_TEMPLATE_LIST_TITLE    = "Here's the available Templates for Backend Frameworks."
 
-	FRONTEND_VUE         = "vue"
+	FRONTEND_VUE         = "🔵 vue"
 	FRONTEND_VUE_DESC    = "Generate Vue.js App Template"
 	FRONTEND_VUE_CMD     = "npm"
 	FRONTEND_VUE_CMD_ARG = "init vite@latest my-vue-app -- --template vue"
 
-	FRONTEND_VUE_TS         = "vue-ts"
+	FRONTEND_VUE_TS         = "🔵 vue-ts"
 	FRONTEND_VUE_TS_DESC    = "Generate Vue.js App Template in TypeScript"
 	FRONTEND_VUE_TS_CMD     = "npm"
 	FRONTEND_VUE_TS_CMD_ARG = "init vite@latest my-vue-typescript-app -- --template vue-ts"
 
-	FRONTEND_REACT         = "react"
+	FRONTEND_REACT         = "🔵 react"
 	FRONTEND_REACT_DESC    = "Generate React App Template"
 	FRONTEND_REACT_CMD     = "npm"
 	FRONTEND_REACT_CMD_ARG = "create-react-app my-react-app"
 
-	FRONTEND_REACT_TS         = "react-ts"
+	FRONTEND_REACT_TS         = "🔵 react-ts"
 	FRONTEND_REACT_TS_DESC    = "Generate React App Template in TypeScript"
 	FRONTEND_REACT_TS_CMD     = "npm"
 	FRONTEND_REACT_TS_CMD_ARG = "create-react-app my-react-typescript-app --typescript"
 
-	FRONTEND_NEXT         = "next"
+	FRONTEND_NEXT         = "🔵 next"
 	FRONTEND_NEXT_DESC    = "Generate Next.js App Template"
 	FRONTEND_NEXT_CMD     = "npm"
 	FRONTEND_NEXT_CMD_ARG = "create-next-app my-next-app"
 
-	FRONTEND_NEXT_TS         = "next-ts"
+	FRONTEND_NEXT_TS         = "🔵 next-ts"
 	FRONTEND_NEXT_TS_DESC    = "Generate Next.js App Template in TypeScript"
 	FRONTEND_NEXT_TS_CMD     = "npm"
 	FRONTEND_NEXT_TS_CMD_ARG = "create-next-app my-next-typescript-app --typescript"
 
-	FRONTEND_VANILLA         = "vanilla"
+	FRONTEND_VANILLA         = "🔵 vanilla"
 	FRONTEND_VANILLA_DESC    = "Generate Vanilla.js App Template"
 	FRONTEND_VANILLA_CMD     = "npm"
 	FRONTEND_VANILLA_CMD_ARG = "init vite@latest my-vanilla-app -- --template vanilla"
 
-	FRONTEND_VANILLA_TS         = "vanilla-ts"
+	FRONTEND_VANILLA_TS         = "🔵 vanilla-ts"
 	FRONTEND_VANILLA_TS_DESC    = "Generate Vanilla.js App Template in TypeScript"
 	FRONTEND_VANILLA_TS_CMD     = "npm"
 	FRONTEND_VANILLA_TS_CMD_ARG = "init vite@latest my-vanilla-typescript-app -- --template vanilla-ts"
 
-	FRONTEND_GATSBY         = "gatsby"
+	FRONTEND_GATSBY         = "🔵 gatsby"
 	FRONTEND_GATSBY_DESC    = "Generate Gatsby App Template"
 	FRONTEND_GATSBY_CMD     = "npm"
 	FRONTEND_GATSBY_CMD_ARG = "init vite@latest my-gatsby-typescript-app -- --template gatsby"
 
-	FRONTEND_GATSBY_TS         = "gatsby-ts"
+	FRONTEND_GATSBY_TS         = "🔵 gatsby-ts"
 	FRONTEND_GATSBY_TS_DESC    = "Generate Gatsby App Template in TypeScript"
 	FRONTEND_GATSBY_TS_CMD     = "npm"
 	FRONTEND_GATSBY_TS_CMD_ARG = "init vite@latest my-gatsby-typescript-app -- --template gatsby-ts"
