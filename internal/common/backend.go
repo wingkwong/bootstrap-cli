e7b47dd844f34059a3aa5c5c5d380e20c72a40a8
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-12 12:34:21 +0800

    init(common): backend.go

---
+++ internal/common/backend.go
@@ -0,0 +1,8 @@
+package common
+
+const (
+	BACKEND_FRAMEWORKS          = "📙 Backend Frameworks"
+	BACKEND_FRAMEWORKS_DESC     = "Explore Backend Framework Templates"
+	BACKEND_TEMPLATE_LIST       = "backend-template-list"
+	BACKEND_TEMPLATE_LIST_TITLE = "Here's the available Templates for Backend Frameworks."
+)
