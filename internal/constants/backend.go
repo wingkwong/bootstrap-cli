706b3079b6dbfd6e1af98a6e082924b4aea913a8
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-13 22:46:28 +0800

    refactor(constants): move to templates

--- internal/constants/backend.go
+++ internal/constants/backend.go
@@ -5,9 +5,4 @@ const (
 	BACKEND_FRAMEWORKS_DESC     = "Explore Backend Framework Templates"
 	BACKEND_TEMPLATE_LIST       = "backend-template-list"
 	BACKEND_TEMPLATE_LIST_TITLE = "Here's the available Templates for Backend Frameworks."
-
-	BACKEND_EXPRESS         = "🟠 express"
-	BACKEND_EXPRESS_DESC    = "Generate Express.js App Template"
-	BACKEND_EXPRESS_CMD     = "npx"
-	BACKEND_EXPRESS_CMD_ARG = "express-generator my-express-app"
 )
