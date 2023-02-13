b8e4aeb1306d68309193f2427a6cbff61dd3eb42
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-13 22:47:08 +0800

    init(templates): backend.go

---
+++ internal/templates/backend.go
@@ -0,0 +1,6 @@
+package template
+
+var BACKEND_TEMPLATES = []Item{
+	{Title: "🔵 express", Desc: "Generate Express.js App Template", Command: "npx", CommandArgs: "express-generator my-express-app"},
+	{Title: "🔵 koa", Desc: "Generate Koa.js App Template", Command: "npx", CommandArgs: "create-koa-application my-koa-app"},
+}
