f07d55b3da350358fd93c89a33ff4daa7a06496f
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-15 22:48:43 +0800

    refactor(templates): remove emojis

--- internal/templates/backend.go
+++ internal/templates/backend.go
@@ -1,6 +1,6 @@
 package template
 
 var BACKEND_TEMPLATES = []Item{
-	{Title: "🟠 express", Desc: "Generate Express.js App Template", Command: "npx", CommandArgs: "express-generator my-express-app"},
-	{Title: "🟠 koa", Desc: "Generate Koa.js App Template", Command: "npx", CommandArgs: "create-koa-application my-koa-app"},
+	{Title: "express", Desc: "Generate Express.js App Template", Command: "npx", CommandArgs: "express-generator my-express-app"},
+	{Title: "koa", Desc: "Generate Koa.js App Template", Command: "npx", CommandArgs: "create-koa-application my-koa-app"},
 }
