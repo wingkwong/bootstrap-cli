6445859fc27d2a39c64810c666840d9a6b9a29ca
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-15 23:45:55 +0800

    feat(constants): add DOCKER_TEMPLATE_LIST & DOCKER_TEMPLATE_LIST_TITLE

--- internal/constants/docker.go
+++ internal/constants/docker.go
@@ -1,6 +1,8 @@
 package constants
 
 const (
-	DOCKER_FRAMEWORKS      = "📒 Docker"
-	DOCKER_FRAMEWORKS_DESC = "Explore Dockerfile"
+	DOCKER_FRAMEWORKS          = "📒 Docker"
+	DOCKER_FRAMEWORKS_DESC     = "Explore Docker Commands"
+	DOCKER_TEMPLATE_LIST       = "docker-template-list"
+	DOCKER_TEMPLATE_LIST_TITLE = "Here's the available commands for Docker"
 )
