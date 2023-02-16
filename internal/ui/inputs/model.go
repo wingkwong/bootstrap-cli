ea4071b9deb9beb55cd7f13ec5b9f4b8e78acf8b
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-16 21:52:02 +0800

    init(inputs): model.go

---
+++ internal/ui/inputs/model.go
@@ -0,0 +1,16 @@
+package inputs
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+)
+
+type Bubble struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode textinput.CursorMode
+	finished   bool
+}
+
+func (b Bubble) IsFinished() bool { return b.finished }
+
+func (b Bubble) getInputs() []textinput.Model { return b.inputs }
