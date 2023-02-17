c7557d2ede71f1c47cf6f745c07caad683d0f9fb
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-17 21:39:02 +0800

    fix(templates): add missing args for next app

--- internal/templates/frontend.go
+++ internal/templates/frontend.go
@@ -5,8 +5,9 @@ var FRONTEND_TEMPLATES = []Item{
 	{Title: "vue-ts", Desc: "Generate Vue.js App Template in TypeScript", Command: "npm", CommandArgs: "init --y vite@latest my-vue-typescript-app -- --template vue-ts"},
 	{Title: "react", Desc: "Generate React.js App Template", Command: "npm", CommandArgs: "init --y vite@latest my-react-app -- --template react"},
 	{Title: "react-ts", Desc: "Generate React.js App Template in TypeScript", Command: "npm", CommandArgs: "init --y vite@latest my-react-typescript-app -- --template react-ts"},
-	{Title: "next", Desc: "Generate Next.js App Template", Command: "npm", CommandArgs: "init --y vite@latest my-next-app -- --template next"},
-	{Title: "next-ts", Desc: "Generate Next.js App Template in TypeScript", Command: "npm", CommandArgs: "init --y vite@latest my-next-typescript-app -- --template next-ts"},
+	// TODO: move to prompt
+	{Title: "next", Desc: "Generate Next.js App Template", Command: "npx", CommandArgs: "--yes create-next-app my-next-app --eslint --src-dir --experimental-app false --use-npm --import-alias '@/*' --js"},
+	{Title: "next-ts", Desc: "Generate Next.js App Template in TypeScript", Command: "npx", CommandArgs: "--y create-next-app my-next-typescript-app --eslint --src-dir --experimental-app false --use-npm --import-alias '@/*' --ts"},
 	{Title: "vanilla", Desc: "Generate Vanilla.js App Template", Command: "npm", CommandArgs: "init --y vite@latest my-vanilla-app -- --template vanilla"},
 	{Title: "vanilla-ts", Desc: "Generate Vanilla.js App Template in TypeScript", Command: "npm", CommandArgs: "init --y vite@latest my-vanilla-typescript-app -- --template vanilla-ts"},
 	{Title: "gatsby", Desc: "Generate Gatsby App Template", Command: "npm", CommandArgs: "init --y vite@latest my-gatsby-app -- --template gatsby"},
