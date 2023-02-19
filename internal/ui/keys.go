2bcc360fc9f987f1f814f15631b26af5540e6f80
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-20 00:14:02 +0800

    refactor(ui): move core logic out

--- internal/ui/keys.go
+++ internal/ui/keys.go
@@ -2,24 +2,6 @@ package ui
 
 import "github.com/charmbracelet/bubbles/key"
 
-type KeyMap struct {
-	Quit           key.Binding
-	Exit           key.Binding
-	SelectListItem key.Binding
-	Back           key.Binding
-	Next           key.Binding
-}
-
-func DefaultKeyMap() KeyMap {
-	return KeyMap{
-		Quit: key.NewBinding(
-			key.WithKeys("ctrl+c"),
-		),
-		Exit: key.NewBinding(
-			key.WithKeys("q"),
-		),
-		SelectListItem: key.NewBinding(
-			key.WithKeys("enter"),
-		),
-	}
-}
+var (
+	selectListItemKey = key.NewBinding(key.WithKeys("enter"))
+)
