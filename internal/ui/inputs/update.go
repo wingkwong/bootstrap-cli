0d29f4e0f3721f3218465925736fe42d22610bd8
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-21 23:05:37 +0800

    feat(inputs): revise inputs update logic

--- internal/ui/inputs/update.go
+++ internal/ui/inputs/update.go
@@ -1,70 +1,57 @@
 package inputs
 
 import (
-	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func (b Bubble) Update(msg tea.Msg) (Bubble, tea.Cmd) {
+	if !b.Active {
+		return b, nil
+	}
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
 
-	if b.Active {
-		switch msg := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String() {
-			// Change cursor mode
-			case "ctrl+r":
-				b.CursorMode++
-				if b.CursorMode > textinput.CursorHide {
-					b.CursorMode = textinput.CursorBlink
-				}
-				cmds := make([]tea.Cmd, len(b.Inputs))
-				for i := range b.Inputs {
-					cmds[i] = b.Inputs[i].SetCursorMode(b.CursorMode)
-				}
-				return b, tea.Batch(cmds...)
-
-			// Set focus to next input
-			case "tab", "shift+tab", "enter", "up", "down":
-				s := msg.String()
-
-				// Did the user press enter while the submit button was focused?
-				// If so, exit.
-				if s == "enter" && b.FocusIndex == len(b.Inputs) {
-					// return b, tea.Quit
-					b.Finished = true
-					return b, nil
-				}
-
-				// Cycle indexes
-				if s == "up" || s == "shift+tab" {
-					b.FocusIndex--
-				} else {
-					b.FocusIndex++
-				}
-
-				if b.FocusIndex > len(b.Inputs) {
-					b.FocusIndex = 0
-				} else if b.FocusIndex < 0 {
-					b.FocusIndex = len(b.Inputs)
-				}
-
-				cmds := make([]tea.Cmd, len(b.Inputs))
-				for i := 0; i <= len(b.Inputs)-1; i++ {
-					if i == b.FocusIndex {
-						// Set focused state
-						cmds[i] = b.Inputs[i].Focus()
-						b.Inputs[i].PromptStyle = focusedStyle
-						b.Inputs[i].TextStyle = focusedStyle
-						continue
-					}
-					// Remove focused state
-					b.Inputs[i].Blur()
-					b.Inputs[i].PromptStyle = noStyle
-					b.Inputs[i].TextStyle = noStyle
-				}
-
-				return b, tea.Batch(cmds...)
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && b.FocusIndex == len(b.Inputs) {
+				// return b, tea.Quit
+				b.Finished = true
+				return b, nil
 			}
+
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				b.FocusIndex--
+			} else {
+				b.FocusIndex++
+			}
+
+			if b.FocusIndex > len(b.Inputs) {
+				b.FocusIndex = 0
+			} else if b.FocusIndex < 0 {
+				b.FocusIndex = len(b.Inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(b.Inputs))
+			for i := 0; i <= len(b.Inputs)-1; i++ {
+				if i == b.FocusIndex {
+					// Set focused state
+					cmds[i] = b.Inputs[i].Focus()
+					b.Inputs[i].PromptStyle = focusedStyle
+					b.Inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				b.Inputs[i].Blur()
+				b.Inputs[i].PromptStyle = noStyle
+				b.Inputs[i].TextStyle = noStyle
+			}
+
+			return b, tea.Batch(cmds...)
 		}
 	}
 
