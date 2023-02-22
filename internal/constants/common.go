e01c376c3dcd10ac04a2d268ffc60df46306470a
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-23 00:37:11 +0800

    init(constants): common.go

---
+++ internal/constants/common.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	APP_NAME     = "Bootstrap CLI"
+	APP_DESC     = "A minimalistic CLI to bootstrap projects with different frameworks"
+	APP_REPO_URL = "https://discord.gg/hGKVsGxMY3"
+)
