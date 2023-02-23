ccb0d449aed966d696d0f3b6ff59568cede81b80
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-02-23 22:08:29 +0800

    refactor(inputs): NewInputModel

--- internal/ui/inputs/app.go
+++ internal/ui/inputs/app.go
@@ -4,63 +4,49 @@ import (
 	"github.com/charmbracelet/bubbles/textinput"
 )
 
-func NewViteInputModel() Bubble {
-	b := Bubble{
-		Inputs: make([]textinput.Model, 2),
-	}
+type inputBubble struct {
+	placeholder string
+	echoMode    textinput.EchoMode
+}
 
+func NewInputModel(data []inputBubble) Bubble {
+	n := len(data)
+	b := Bubble{
+		Inputs: make([]textinput.Model, n),
+	}
 	var t textinput.Model
 	for i := range b.Inputs {
 		t = textinput.New()
 		t.CursorStyle = cursorStyle
 		t.CharLimit = 32
-
-		switch i {
-		case 0:
-			t.Placeholder = "Enter App Name"
+		if i == 0 {
 			t.Focus()
 			t.PromptStyle = focusedStyle
 			t.TextStyle = focusedStyle
-		case 1:
-			t.Placeholder = "Enter the directory"
+		}
+		t.Placeholder = data[i].placeholder
+		t.EchoMode = data[i].echoMode
+		if data[i].echoMode == 2 {
+			t.EchoCharacter = '•'
 		}
 		b.Inputs[i] = t
 	}
-
 	return b
 }
 
+func NewViteInputModel() Bubble {
+	return NewInputModel([]inputBubble{
+		{placeholder: "Enter App Name", echoMode: textinput.EchoNormal},
+		{placeholder: "Enter the directory", echoMode: textinput.EchoNormal},
+	})
+}
+
 func NewMSSQLInputModel() Bubble {
-	b := Bubble{
-		Inputs: make([]textinput.Model, 5),
-	}
-
-	var t textinput.Model
-	for i := range b.Inputs {
-		t = textinput.New()
-		t.CursorStyle = cursorStyle
-		t.CharLimit = 32
-
-		switch i {
-		case 0:
-			t.Placeholder = "MSSQL_USER"
-			t.Focus()
-			t.PromptStyle = focusedStyle
-			t.TextStyle = focusedStyle
-		case 1:
-			t.Placeholder = "MSSQL_SA_PASSWORD"
-			t.EchoMode = textinput.EchoPassword
-			t.EchoCharacter = '•'
-		case 2:
-			t.Placeholder = "MSSQL_PID"
-		case 3:
-			t.Placeholder = "name"
-		case 4:
-			t.Placeholder = "port"
-		}
-
-		b.Inputs[i] = t
-	}
-
-	return b
+	return NewInputModel([]inputBubble{
+		{placeholder: "MSSQL_USER", echoMode: textinput.EchoNormal},
+		{placeholder: "MSSQL_SA_PASSWORD", echoMode: textinput.EchoNormal},
+		{placeholder: "MSSQL_PID", echoMode: textinput.EchoPassword},
+		{placeholder: "name", echoMode: textinput.EchoNormal},
+		{placeholder: "port", echoMode: textinput.EchoNormal},
+	})
 }
