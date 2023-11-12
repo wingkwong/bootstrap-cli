8d9237d4172738b4a1302e455761797be0dda97b
tobigiwa <giwaoluwatobi@gmail.com>
2023-11-12 08:39:44 -0500

    cobra launch command for bubbletea TUI. included the launch cobra commad to display the TUI bubbletea view. Modified cmd/root.go and included file license.
    Signed-off-by: tobigiwa <giwaoluwatobi@gmail.com>

---
+++ cmd/launch.go
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023, Oluwatobi Giwa
+ * All rights reserved.
+ *
+ * This software is licensed under the MIT License.
+ * See the LICENSE file or visit https://opensource.org/licenses/MIT for details.
+ */
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/wingkwong/bootstrap-cli/internal/ui"
+)
+
+// launchCmd represents the launch command
+var launchCmd = &cobra.Command{
+	Use:     "launch",
+	Short:   "launches text user interface view",
+	Long:    "launches the TUI (text user interface) view for the application",
+	Aliases: []string{"tui"},
+	Run: func(cmd *cobra.Command, args []string) {
+		ui.Execute()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(launchCmd)
+}
