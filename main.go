ae6ac7eaaacb39f76cf1e211b41829f9cfb96e61
tobigiwa <giwaoluwatobi@gmail.com>
2023-11-16 09:19:19 +0100

    Resolves #6 removed license from .go files
    Signed-off-by: tobigiwa <giwaoluwatobi@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2023, Oluwatobi Giwa
- * All rights reserved.
- *
- * This software is licensed under the MIT License.
- * See the LICENSE file or visit https://opensource.org/licenses/MIT for details.
- */
 package main
 
 import "github.com/wingkwong/bootstrap-cli/cmd"
